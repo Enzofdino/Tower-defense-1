13734d607abb446db15b393c994719e2bd7511ac
Enzofdino <enzofhostin@gmail.com>
2024-10-22 11:38:04 -0300

    continuando o script do torret

--- defesa de torre/Assets/Script/Turret.cs
+++ defesa de torre/Assets/Script/Turret.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,6 +10,7 @@ public class Turret : MonoBehaviour
     [SerializeField] private Transform turretrotation;
     [Header("Attribute")]
     [SerializeField] private float targetingrange = 5f;
+    [SerializeField] private LayerMask enemymask;
 
     private Transform target;
 
@@ -17,11 +19,32 @@ public class Turret : MonoBehaviour
         if(target != null)
         {
             Findtarget();
+            return;
         }
+        RotateTowardsTarget();
+        checktargetisrange();
     }
     private void Findtarget()
     {
-        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingrange, (Vector2)transform.position, 0f, enemyMask);
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingrange, (Vector2)
+            transform.position, 0f, enemymask);
+
+        if(hits.Length > 0 )
+        {
+            target = hits[0].transform;
+        }
+    }
+    private bool checktargetisrange()
+    {
+        return Vector2.Distance(target.position,transform.position) <= targetingrange;
+    }
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y - transform.position.y, target.position.x 
+            - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f,0f,angle));
+        turretrotation.rotation = targetRotation;
     }
     private void OnDrawGizmosSelected()
     {
